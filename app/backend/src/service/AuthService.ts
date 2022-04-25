4886562bd1c5d68e49d456776c54b58122b420da
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2022-04-25 19:29:38 -0300

    feat: verify token

--- app/backend/src/service/AuthService.ts
+++ app/backend/src/service/AuthService.ts
@@ -3,11 +3,22 @@ import { readFile } from 'fs/promises';
 import { compare } from 'bcryptjs';
 import User from '../database/models/User';
 import IAuth from '../Interfaces/Auth';
+import IAuthVerify from '../Interfaces/AuthVerify';
 
 export default class AuthService {
-  static async auth(email: string, pass: string): Promise<null | IAuth> {
+  private _secret: string;
+
+  constructor() {
+    this.secret();
+  }
+
+  private async secret() {
     const SECRET = await readFile('jwt.evaluation.key', 'utf8');
 
+    this._secret = SECRET;
+  }
+
+  async auth(email: string, pass: string): Promise<null | IAuth> {
     const user = await User.findOne({ where: { email } });
 
     if (!user) return null;
@@ -20,14 +31,24 @@ export default class AuthService {
     };
 
     const isValidPass = await compare(pass, user.password);
-    // const isValidPass = user.password === pass;
 
     if (!isValidPass) return null;
 
     const signOptions: jwt.SignOptions = { algorithm: 'HS256', expiresIn: '1d' };
 
-    const token = jwt.sign({ data: dataUser }, SECRET, signOptions);
+    const token = jwt.sign({ data: dataUser }, this._secret, signOptions);
 
     return { user: dataUser, token };
+  }
+
+  verify(token: string) {
+    try {
+      const result = jwt.verify(token, this._secret);
+      const { data: { role } } = result as IAuthVerify;
+
+      return role;
+    } catch (error) {
+      return false;
+    }
   }
 }

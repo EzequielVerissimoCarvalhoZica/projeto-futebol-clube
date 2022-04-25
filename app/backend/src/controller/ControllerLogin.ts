56b3fe38d378f5852dc029bd394fadd8f6f34096
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2022-04-25 19:32:14 -0300

    feat: method validate controller login

--- app/backend/src/controller/ControllerLogin.ts
+++ app/backend/src/controller/ControllerLogin.ts
@@ -3,13 +3,17 @@ import { HttpErrorStatusCode } from '../helper';
 import AuthService from '../service/AuthService';
 
 export default class ControllerLogin {
+  private _AuthService = new AuthService();
+
   login = async (req: Request, res: Response, next: NextFunction) => {
     const { email, password } = req.body;
 
-    const result = await AuthService.auth(email, password);
+    const result = await this._AuthService.auth(email, password);
 
     if (!result) return next(new HttpErrorStatusCode('Incorrect email or password', 401));
 
     return res.status(200).json(result);
   };
+
+  validate = (req: Request, res: Response) => res.status(200).send(req.role);
 }

6e38bacc1da4a004f69b895bf26c566c10da682e
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2022-04-26 18:33:46 -0300

    feat: controller method create match and finishMatch

--- app/backend/src/controller/ControllerMatches.ts
+++ app/backend/src/controller/ControllerMatches.ts
@@ -15,4 +15,30 @@ export default class ControllerMatches {
     const matches = await this._ServiceMatches.findAll();
     return res.status(200).json(matches);
   };
+
+  public create = async (req: Request, res: Response) => {
+    const { homeTeam, awayTeam, homeTeamGoals, awayTeamGoals, inProgress } = req.body;
+
+    if (homeTeam === awayTeam) {
+      return res.status(400)
+        .json({ message: 'It is not possible to create a match with two equal teams' });
+    }
+
+    const match = await this._ServiceMatches
+      .create({ homeTeam, awayTeam, homeTeamGoals, awayTeamGoals, inProgress });
+
+    return res.status(201).json(match);
+  };
+
+  public finishMatch = async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    const match = await this._ServiceMatches.finishMatch(Number(id));
+
+    if (match[0] === 1) {
+      return res.status(200).json({ message: 'The referee ends the match successfully' });
+    }
+
+    return res.status(400).json({ message: 'Error in end match' });
+  };
 }

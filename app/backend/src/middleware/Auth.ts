e6a1479d9f429193162cfc0fc2d1095b99fd900f
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2022-04-26 15:37:34 -0300

    fix: implements interface request

--- app/backend/src/middleware/Auth.ts
+++ app/backend/src/middleware/Auth.ts
@@ -1,10 +1,11 @@
-import { Request, Response, NextFunction } from 'express';
+import { Response, NextFunction } from 'express';
+import { IRequest } from '../Interfaces';
 import { AuthService } from '../service';
 
 export default class Auth {
   constructor(private _AuthService: AuthService) {}
 
-  auth = async (req: Request, res: Response, next: NextFunction) => {
+  auth = async (req: IRequest, res: Response, next: NextFunction) => {
     const { authorization } = req.headers;
 
     if (!authorization) return res.status(400).json({ message: 'token not found' });

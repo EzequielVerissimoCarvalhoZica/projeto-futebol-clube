7f4ae85147dc99f36fcb37efae1540fa3b2f22d3
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2022-05-02 16:34:24 -0300

    feat: helper func order leader board

--- app/backend/src/helper/index.ts
+++ app/backend/src/helper/index.ts
@@ -1,7 +1,9 @@
 import HttpErrorStatusCode from './error/HttpErrorStatusCode';
 import joiLogin from './schema/joiLogin';
+import orderLeaderBoard from './schema/orderLeaderBoard';
 
 export {
   HttpErrorStatusCode,
   joiLogin,
+  orderLeaderBoard,
 };
